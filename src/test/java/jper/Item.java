89127795f4f81cd628fdbf754050334dfaaef9a0
21503800 <jeff_perio@hotmail.fr>
2016-02-24 11:52:54 +0100

    #3 Implantation des test pour SimpleStackImpl

--- src/test/java/jper/Item.java
+++ src/test/java/jper/Item.java
@@ -1,8 +1,5 @@
 package jper;
 
-/**
- * Created by 21503800 on 24/02/2016.
- */
 public class Item
 {
     private int value;
@@ -18,5 +15,10 @@ public class Item
     {
         value = 0;
         name = "#0";
+    }
+
+    public int getValue()
+    {
+        return value;
     }
 }
--- src/test/java/jper/test/SimpleStackImplTest.java
+++ src/test/java/jper/test/SimpleStackImplTest.java
@@ -1,45 +1,87 @@
 package jper.test;
 
+import jper.Item;
+import jper.SimpleStack;
+import jper.SimpleStackImpl;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class SimpleStackImplTest {
 
-    @Before
-    public void setUp() throws Exception {
+    SimpleStack simpleStack;
 
+    @Before
+    public void setUp() throws Exception
+    {
+        simpleStack = new SimpleStackImpl();
+        System.out.println("Je suis executé AVANT chaque test.");
     }
 
     @After
-    public void tearDown() throws Exception {
+    public void tearDown() throws Exception
+    {
+        System.out.println("Je suis executé APRES chaque test.");
+    }
+
+    @Test
+    public void testIsEmpty() throws Exception
+    {
+        Assert.assertEquals(true,simpleStack.isEmpty());
+        simpleStack.push(new Item(42));
+        Assert.assertEquals(false,simpleStack.isEmpty());
+    }
+
+    @Test
+    public void testGetSize() throws Exception
+    {
+        Assert.assertEquals(0,simpleStack.getSize());
+        simpleStack.push(new Item(42));
+        Assert.assertEquals(1,simpleStack.getSize());
+        simpleStack.push(new Item(445));
+        Assert.assertEquals(2,simpleStack.getSize());
+    }
+
+    @Test
+    public void testPush() throws Exception
+    {
+        Item item1 = new Item(45);
+        Assert.assertEquals(0,simpleStack.getSize());
+        simpleStack.push(item1);
+        Assert.assertEquals(1,simpleStack.getSize());
+        Item item2 = simpleStack.peek();
+        Assert.assertEquals(item2.getValue(),45);
+        Assert.assertEquals(item2.getValue(),item1.getValue());
 
     }
 
     @Test
-    public void testIsEmpty() throws Exception {
+    public void testPeek() throws Exception
+    {
+        Item item1 = new Item(45);
+        Assert.assertEquals(0,simpleStack.getSize());
+        simpleStack.push(item1);
+        Assert.assertEquals(1,simpleStack.getSize());
+        Item item2 = simpleStack.peek();
+        Assert.assertEquals(1,simpleStack.getSize());
+        Assert.assertEquals(item2.getValue(),45);
+        Assert.assertEquals(item2.getValue(),item1.getValue());
 
     }
 
     @Test
-    public void testGetSize() throws Exception {
-
-    }
-
-    @Test
-    public void testPush() throws Exception {
-
-    }
-
-    @Test
-    public void testPeek() throws Exception {
-
-    }
-
-    @Test
-    public void testPop() throws Exception {
+    public void testPop() throws Exception
+    {
+        Item item1 = new Item(45);
+        Assert.assertEquals(0,simpleStack.getSize());
+        simpleStack.push(item1);
+        Assert.assertEquals(1,simpleStack.getSize());
+        Item item2 = simpleStack.pop();
+        Assert.assertEquals(0,simpleStack.getSize());
+        Assert.assertEquals(1,simpleStack.getSize());
+        Assert.assertEquals(item2.getValue(),45);
+        Assert.assertEquals(item2.getValue(),item1.getValue());
 
     }
 }
