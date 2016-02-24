0c886ec4a208de51c6fcefb22d2c4a34db67d088
21503800 <jeff_perio@hotmail.fr>
2016-02-24 11:11:47 +0100

    #1 Commit de test pour le lier a l'exigence numero 1

--- src/test/java/jper/Item.java
+++ src/test/java/jper/Item.java
@@ -6,14 +6,17 @@ package jper;
 public class Item
 {
     private int value;
+    public String name;
 
     public Item(int init)
     {
         value = init;
+        name = "#"+init;
     }
 
     public Item()
     {
         value = 0;
+        name = "#0";
     }
 }
